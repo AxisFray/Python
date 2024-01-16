c9921e187c47fa454b90aaf4d12adf5b3c501945
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-01-16 15:32:43 +0100

    Add files via upload

--- klasy.py
+++ klasy.py
@@ -19,19 +19,30 @@ tab = []
 class Wojownik:
     def __init__(self,imie):
         self.imie=imie
-        self.sila =sila
-        self.szybkosc=szybkosc
-        self.jedzenie = jedzenie
+        self.sila = random.randint(0,3)
+        self.szybkosc=random.randint(0,3)
+        self.jedzenie = random.randint(0,3)
 
-    szybkosc = random.randint(0,3)
-    sila=random.randint(0,3)
-    jedzenie= random.randint(0,3)
     def atak(self):
-        pass
+        if tab[0] ==self.imie:
+            suma=0
+            suma = tab[1].sila + tab[1].szybkosc + tab[1].jedzenie
+            suma1 = self.sila+self.szybkosc + self.jedzenie
+            if suma>suma1:
+                self.smierc()
+            else : self.zwyciestwo()
+    
+    def smierc(self):
+        print(f"Umarles {self.imie}")
+    def zwyciestwo(self):
+        print(f"Zwyciezasz!  {self.imie}")
 
 
-W1 = Wojownik("Janek")
+tab.append(Wojownik("Franek"))
+tab.append(Wojownik("Janek"))
 
+
+tab[0].atak()
 
 
 
