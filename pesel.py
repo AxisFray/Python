e706e9581f2ad828e75c8c8d75736c8f04fad11a
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-01-18 11:12:49 +0100

    Add files via upload

--- pesel.py
+++ pesel.py
@@ -1,11 +1,11 @@
-
+import random
 
 
 # przed 200r
 #  RR MM DD LLL P K
 # L liczba porzadkowa
 # P plec parzysta kobieta
-# k cyfra kontrolna
+# k cyfra kontrolna 1-3-7-9-1-3-7-9-1-3
 #po 2000r
 # dodac 20 do miesiaca
 #
@@ -13,5 +13,20 @@
 #
 
 
-def GenPesel(plec,rok,):
+def GenPesel():
+    while plec<0 or plec>100:
+        plec = int(input("Podaj plec - parzysta=kobieta, nieparzysta=mezczyzna"))
+    rok = int(input("Podaj  rok urodzenia"))
+    while miesiac<0:
+        miesiac = int(input("Podaj miesiac urodzenia"))
+    while dzien<0 or dzien>32:
+        dzien = int(input("Podaj dzien urodzenia"))
+    pesel= ""
+    miesiac = str(miesiac)
+    if rok>2000:
+        miesiac+=20
+    pesel+= rok[2]+rok[3]+miesiac+dzien
+    for i in range(0,3):
+        x = random.randint(0,9)
+        pesel += str(x)
     pass
