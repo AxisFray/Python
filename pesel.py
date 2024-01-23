1767df3063fbc3d6611b0c7307a2357db7758f97
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-01-23 14:30:29 +0100

    Update pesel.py

--- pesel.py
+++ pesel.py
@@ -15,6 +15,7 @@ def SprawdzCyfry(text):
     for i in text:
         if i!= '1' or i!= '2' or i!= '3' or i!= '4' or i!= '5' or i!= '6' or i!= '7' or i!= '8' or i!= '9' or i!='0':
             return False
+        else: return True
 
 def GenPesel():
     plec = int(input("Podaj plec - parzysta=kobieta, nieparzysta=mezczyzna"))
